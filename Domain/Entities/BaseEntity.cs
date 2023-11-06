4262d157f11616d28bbfde1f1a91638490c4e05d
Christian-M-Silva <tininjacms@gmail.com>
2023-11-06 06:41:28 -0300

    feat(EstuturaInicial): Criará a estrutura inicial do projeto e o baseEntity

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+		private DateTime? _createdAt;
+		public DateTime? CreatedAt
+        {
+			get { return _createdAt; }
+			set { _createdAt = value == null ? DateTime.UtcNow : value; }
+		}
+
+        public DateTime? UpdateAt { get; set; }
+    }
+}
