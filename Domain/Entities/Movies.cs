50dc1d026c8a2bdae96a65a6be41538b473ae5df
Christian Silva <tininjacms@gmail.com>
2023-11-16 06:01:21 -0300

    feat(entites): Criação do Model de Movies

---
+++ Domain/Entities/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Movies
+    {
+        public string Name { get; set; }
+        public float Assessment { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Comments { get; set; }
+    }
+}
